845811e80dc7d29fe019a5e057b8acc518462439
Sarvagnya Purohit <saRvaGnyA@users.noreply.github.com>
2022-03-24 19:57:25 +0530

    company routes done

--- postgres/pgConnect.js
+++ postgres/pgConnect.js
@@ -1,19 +1,19 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-// const pool = new Pool({
-//   user: process.env.PG_USER,
-//   password: process.env.PG_PW,
-//   host: process.env.PG_HOST,
-//   port: process.env.PG_PORT,
-//   database: process.env.PG_DB,
-// });
-
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  user: process.env.PG_USER,
+  password: process.env.PG_PW,
+  host: process.env.PG_HOST,
+  port: process.env.PG_PORT,
+  database: process.env.PG_DB,
 });
+
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false,
+//   },
+// });
 
 module.exports = pool;
--- routes/auth.js
+++ routes/auth.js
@@ -10,9 +10,22 @@ const {
   getUserData,
 } = require('../controllers/controller-auth');
 
+const {
+  registerCompany,
+  loginCompany,
+  getCompanyData,
+} = require('../controllers/controller-company');
+
 // ROUTES
+
+// for user
 router.route('/register-user').post(registerUser);
 router.route('/login-user').post(loginUser);
 router.post('/verify-user', jwtAuthorization, getUserData);
+
+// for company
+router.route('/register-company').post(registerCompany);
+router.route('/login-company').post(loginCompany);
+router.post('/verify-company', jwtAuthorization, getCompanyData);
 
 module.exports = router;
